ae26e27ed8d46794738ed4661f683fe4a82528fd
Humberto Rivero <humbertorivero9@gmail.com>
2023-10-24 12:10:05 -0300

    Liskov resolved

--- modulo-3/liskov/src/App.tsx
+++ modulo-3/liskov/src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Button } from './components/Button';
+import { ButtonAlert } from './components/ButtonAlert';
+
 
 // Si creamos un componente que sea RedButton y por ejemplo que herede el comportamiento de Button (ojo no estamos trabajando con clases), este debemos poder cambiarlo por Button en cualquier parte de la app, respetando la sustitucion de Liskov
 function App() {
 	return (
 		<div className="App">
       <h1>Principio de sustitución de Liskov (LSP) 👀</h1>
-      <Button color="secondary" size="medium">Button</Button>
+			<Button color="primary" size="medium">Button</Button>
+			<ButtonAlert />
 		</div>
 	);
 }
---
+++ modulo-3/liskov/src/components/ButtonAlert/ButtonAlert.tsx
@@ -0,0 +1,7 @@
+import Button from '../Button/Button'
+
+export const ButtonAlert = () => {
+  return (
+    <Button color="secondary" size="medium">Button</Button>
+  )
+}
