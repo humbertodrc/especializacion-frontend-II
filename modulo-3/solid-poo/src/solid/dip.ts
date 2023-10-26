8dee3236a87f158f980c78cc5d5c2479fef90de0
Humberto Rivero <humbertorivero9@gmail.com>
2023-10-26 18:37:56 -0300

    Add explanation examples

--- modulo-3/solid-poo/src/solid/dip.ts
+++ modulo-3/solid-poo/src/solid/dip.ts
@@ -2,6 +2,7 @@
 // El DIP establece que las clases de alto nivel no deben depender de las clases de bajo nivel, sino de abstracciones.
 
 // Mal ejemplo: Violando el DIP
+// En el mal ejemplo, la clase Interruptor depende directamente de la clase concreta Luz. Esto viola el DIP, ya que las clases de alto nivel (como Interruptor) no deben depender de las clases de bajo nivel. Esto hace que Interruptor sea rígido y no permite una fácil sustitución de la implementación de Luz
 class Luz {
   encender() {
     // Implementación de encendido
