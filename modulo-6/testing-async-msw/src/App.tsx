493cd7a2ba1868268a08fc12111aa5d7bc976677
Humberto Rivero <humbertorivero9@gmail.com>
2023-11-20 12:07:10 -0300

    Refactor code and add search functionality

--- modulo-6/testing-async-msw/src/App.tsx
+++ modulo-6/testing-async-msw/src/App.tsx
@@ -1,40 +1,44 @@
 import {useEffect, useState} from "react";
 import "./App.css";
-import { getCharacters } from './service/getCharacters';
-import { Character } from './types/character.types';
+import {getCharacters} from "./service/getCharacters";
+import {Character} from "./types/character.types";
 
 function App() {
 	const [characters, setCharacters] = useState<Character[]>([]);
-	// const [error, setError] = useState(false);
-	// const [loading, setLoading] = useState(false);
+	const [search, setSearch] = useState<string>("");
 
-	// const getCharacters = async () => {
-	// 	setLoading(true);
-	// 	try {
-	// 		const response = await fetch("https://rickandmortyapi.com/api/character");
-	// 		if (!response.ok) throw new Error("Error en la llamada");
-	// 		const data = await response.json();
-	// 		setCharacters(data.results);
-	// 	} catch (error) {
-	// 		setError(true);
-	// 	} finally {
-	// 		setLoading(false);
-	// 	}
-	// };
+	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearch(event.target.value);
+	};
 
 	useEffect(() => {
-		getCharacters().then((data) => setCharacters(data))
-	}, []);
+		const fetchCharacters = async () => {
+			try {
+				const data = await getCharacters(search);
+				setCharacters(data);
+			} catch (error) {
+				console.log("Error fetching characters:", error);
+			}
+		}
+
+		fetchCharacters();
+	}, [search]);
 
 	return (
 		<div className="App">
 			<div>
 				<h1>Rick and Morty</h1>
-				{/* {loading && <p>Loading...</p>}
-				{error && <p>Error</p>} */}
-				<div className='grilla'>
+				<div className='search'>
+					<input
+						type="text"
+						placeholder="Search..."
+						value={search}
+						onChange={handleSearch}
+					/>
+				</div>
+				<div className="grilla">
 					{characters.map((character: any) => (
-						<div key={character.id} className='card'>
+						<div key={character.id} className="card">
 							<h2>{character.name}</h2>
 							<img src={character.image} alt={character.name} />
 						</div>
